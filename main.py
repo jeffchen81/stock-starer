8f4a1f405d8d955103a23f23f7b2c45fbe415b34
陈庆 <cq39882@ly.com>
2018-10-26 11:24:17 +0800

    rename main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from bottle import route, run, template
 
+
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+
 run(host='localhost', port=8080)
+
+
+def main():
+    '''
+    主函数
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    main()
